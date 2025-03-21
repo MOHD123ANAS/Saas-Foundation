ae34f4323e513b30590849bdddec5f2127bd6672
Syraenergy1234 <aws@winspire.io>
2025-03-22 01:28:41 +0530

    Initial commit

---
+++ src/cfehome/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+import pathlib
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "My Page"
+    my_context = {
+        "page_title" : my_title,
+        "page_visit_count":page_qs.count(),
+        "total_page_visit_count":qs.count(),
+        "percent":(page_qs.count()*100) / qs.count()
+    }
+    path = request.path
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template,my_context)
+
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title" : my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+<html lang="en">
+<body>
+<h1>This is {page_title} Anything ? </h1>
+
+</body>
+</html>
+    """. format(**my_context)
+    # print(this_dir)
+    # html_ = ''
+    # html_file_path = this_dir/ "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
