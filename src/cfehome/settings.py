62190a7b9fb498b33a29bf0d54280596eecd1e42
MOHD123ANAS <mohammedanas18025@gmail.com>
2025-03-22 17:40:15 +0530

    prepare for railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,8 +25,13 @@ SECRET_KEY = 'django-insecure-%k+h=oxc=3g13_%qn9bgw(^)bdu*i53!uk(kt@9p%kymztlk=t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [".railway.app" # https://saas.prod.railway.app
+                 ]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -6,23 +6,31 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request,*args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100) / qs.count()
+
+    except:
+        percent = 0
     my_title = "My Page"
     my_context = {
-        "page_title" : my_title,
-        "page_visit_count":page_qs.count(),
-        "total_page_visit_count":qs.count(),
-        "percent":(page_qs.count()*100) / qs.count()
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "total_page_visit_count": qs.count(),
+        "percent": percent
     }
     path = request.path
     html_template = "home.html"
     PageVisit.objects.create(path=request.path)
-    return render(request, html_template,my_context)
+    return render(request, html_template, my_context)
 
-
-def my_old_home_page_view(request, *args, **kwargs):
+def my_old_home_page_visit(request,*args,**kwargs):
     my_title = "My Page"
     my_context = {
         "page_title" : my_title
@@ -41,3 +49,4 @@ def my_old_home_page_view(request, *args, **kwargs):
     # html_file_path = this_dir/ "home.html"
     # html_ = html_file_path.read_text()
     return HttpResponse(html_)
+
